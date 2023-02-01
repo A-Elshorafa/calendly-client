e10a000c9b50ccd8033f1d2ae0eb2ce3fe35184f
a.elshorafa <a.emad@yodawy.com>
2023-02-01 05:15:46 +0200

    Define configurations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        pathname: '/img/**',
+        hostname: 'tailwindui.com'
+      },
+      {
+        protocol: 'http',
+        pathname: '/assets/**',
+        hostname: '**.calendly.com'
+      }
+    ],
+  },
 }
 
 module.exports = nextConfig
